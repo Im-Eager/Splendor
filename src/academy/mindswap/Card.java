c3ea9c877cb1b19b101f54d28e3e3284536cc26a
Ricardo Paiva <ricardowrc@gmail.com>
2022-02-21 16:48:54 +0000

    Add Card Class Reviewed

--- src/academy/mindswap/Card.java
+++ src/academy/mindswap/Card.java
@@ -3,14 +3,31 @@ package academy.mindswap;
 public class Card {
     private int cost;
     private int points;
-    String color;
-    String tear; //levels High, Medium, Low
+    private String color;
+    private int tier; //levels High 3, Medium 2, Low 1
 
-    public Card(int cost, int points, String color, String tear){
+    public Card(int cost, int points, String color, int tie){
         this.cost = cost;
         this.points = points;
         this.color = color;
-        this.tear = tear;
+        this.tier = tier;
     }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
 
 }
