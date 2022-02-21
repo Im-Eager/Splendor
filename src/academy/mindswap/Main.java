f0a5b81039b573718c867e03ce73f1737647a644
Luís Faria <ptluisfaria@gmail.com>
2022-02-21 16:25:29 +0000

    luis added Stuff

--- src/academy/mindswap/Main.java
+++ src/academy/mindswap/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Estou aqio");
         System.out.println("WELCOME TO THE GAME");
+        System.out.println("Luis is here");
     }
 
 }
