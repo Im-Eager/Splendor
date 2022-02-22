efc2b2b2257954107f7c54facbee3ad0b0bec6a5
Im-Eager <83980495+Im-Eager@users.noreply.github.com>
2022-02-22 09:54:35 +0000

    Moved ASCI Arto to class messages

--- src/academy/mindswap/Main.java
+++ src/academy/mindswap/Main.java
@@ -5,34 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //2 Versions to chose
 
-        //Version 1:
-        System.err.println("          /$$$$$$            /$$                           /$$                            ");
-        System.err.println("         /$$__  $$          | $$                          | $$                            ");
-        System.err.println("        | $$  \\__/  /$$$$$$ | $$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$         ");
-        System.err.println(" /$$$$$$|  $$$$$$  /$$__  $$| $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$$$$$");
-        System.err.println("|______/ \\____  $$| $$  \\ $$| $$| $$$$$$$$| $$  \\ $$| $$  | $$| $$  \\ $$| $$  \\__/|______/");
-        System.err.println("         /$$  \\ $$| $$  | $$| $$| $$_____/| $$  | $$| $$  | $$| $$  | $$| $$              ");
-        System.err.println("        |  $$$$$$/| $$$$$$$/| $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/| $$              ");
-        System.err.println("         \\______/ | $$____/ |__/ \\_______/|__/  |__/ \\_______/ \\______/ |__/              ");
-        System.err.println("                  | $$                                                                    ");
-        System.err.println("                  | $$                                                                    ");
-        System.err.println("                  |__/                                                                    ");
-
-
-        //Version 2:
-        System.err.println("  /$$$$$$            /$$                           /$$                   ");
-        System.err.println(" /$$__  $$          | $$                          | $$                    ");
-        System.err.println("| $$  \\__/  /$$$$$$ | $$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ ");
-        System.err.println("|  $$$$$$  /$$__  $$| $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$");
-        System.err.println(" \\____  $$| $$  \\ $$| $$| $$$$$$$$| $$  \\ $$| $$  | $$| $$  \\ $$| $$  \\__/");
-        System.err.println(" /$$  \\ $$| $$  | $$| $$| $$_____/| $$  | $$| $$  | $$| $$  | $$| $$      ");
-        System.err.println("|  $$$$$$/| $$$$$$$/| $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/| $$      ");
-        System.err.println(" \\______/ | $$____/ |__/ \\_______/|__/  |__/ \\_______/ \\______/ |__/      ");
-        System.err.println("          | $$                                                            ");
-        System.err.println("          | $$                                                            ");
-        System.err.println("          |__/                                                            ");
 
     }
 }
--- src/academy/mindswap/utils/Messages.java
+++ src/academy/mindswap/utils/Messages.java
@@ -26,5 +26,35 @@ public class Messages {
         System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
         System.out.println(ANSI_YELLOW + "This text is yellow!" + ANSI_RESET);
 
+
+        //2 Versions to chose
+
+        //Version 1:
+        System.err.println("          /$$$$$$            /$$                           /$$                            ");
+        System.err.println("         /$$__  $$          | $$                          | $$                            ");
+        System.err.println("        | $$  \\__/  /$$$$$$ | $$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$         ");
+        System.err.println(" /$$$$$$|  $$$$$$  /$$__  $$| $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$$$$$");
+        System.err.println("|______/ \\____  $$| $$  \\ $$| $$| $$$$$$$$| $$  \\ $$| $$  | $$| $$  \\ $$| $$  \\__/|______/");
+        System.err.println("         /$$  \\ $$| $$  | $$| $$| $$_____/| $$  | $$| $$  | $$| $$  | $$| $$              ");
+        System.err.println("        |  $$$$$$/| $$$$$$$/| $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/| $$              ");
+        System.err.println("         \\______/ | $$____/ |__/ \\_______/|__/  |__/ \\_______/ \\______/ |__/              ");
+        System.err.println("                  | $$                                                                    ");
+        System.err.println("                  | $$                                                                    ");
+        System.err.println("                  |__/                                                                    ");
+
+
+        //Version 2:
+        System.err.println("  /$$$$$$            /$$                           /$$                   ");
+        System.err.println(" /$$__  $$          | $$                          | $$                    ");
+        System.err.println("| $$  \\__/  /$$$$$$ | $$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ ");
+        System.err.println("|  $$$$$$  /$$__  $$| $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$");
+        System.err.println(" \\____  $$| $$  \\ $$| $$| $$$$$$$$| $$  \\ $$| $$  | $$| $$  \\ $$| $$  \\__/");
+        System.err.println(" /$$  \\ $$| $$  | $$| $$| $$_____/| $$  | $$| $$  | $$| $$  | $$| $$      ");
+        System.err.println("|  $$$$$$/| $$$$$$$/| $$|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/| $$      ");
+        System.err.println(" \\______/ | $$____/ |__/ \\_______/|__/  |__/ \\_______/ \\______/ |__/      ");
+        System.err.println("          | $$                                                            ");
+        System.err.println("          | $$                                                            ");
+        System.err.println("          |__/                                                            ");
+
     }
 }
