4465c8ee4780a42fca1140dd7de30c14f4d35100
Luís Faria <ptluisfaria@gmail.com>
2022-02-23 14:41:07 +0000

    grabGems method updated

--- src/academy/mindswap/game/Player.java
+++ src/academy/mindswap/game/Player.java
@@ -1,7 +1,9 @@
 package academy.mindswap.game;
 
 import academy.mindswap.cards.Card;
+import academy.mindswap.utils.Messages;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Player {
@@ -14,82 +16,96 @@ public class Player {
     private boolean playerCanBuy;
 
 
-    public Player(String name){
+    public Player(String name) {
         this.name = name;
         this.score = 0;
-        this.ownedMines = new int[]{0,0,0,0,0};
-        this.bank = new int[]{0,0,0,0,0,0};
+        this.ownedMines = new int[]{0, 0, 0, 0, 0};
+        this.bank = new int[]{0, 0, 0, 0, 0, 0};
         //Position of bank index[0] = (W)hite, index[1] = Bl(U)e, index[2] = (G)reen, index[3] = (R)ed, index[4] = Blac(K), index[5] = Go(L)d
 
     }
 
-    public void reserveCard(Card card){
-        this.bank[5]+=1;
+    public void reserveCard(Card card) {
+        if( playerReservedCards.size() == 3){
+            System.out.println(Messages.CANT_RESERVE);
+        }
+        this.bank[5] += 1;
         playerReservedCards.add(card);
     }
 
 
-    public void grabGems(int[] grabbedGems){
+    public void grabGems(int[] grabbedGems) {
+
+        int[] temp = bank;
+
         for (int i = 0; i < grabbedGems.length; i++) {
-            bank[i] += grabbedGems[i];
+            temp[i] += grabbedGems[i];
         }
+        int bankTotal = Arrays.stream(temp).reduce(0, Integer::sum);
+
+        if (bankTotal > 10){
+            System.out.println(Messages.MORE_THAN_10);
+            return;
+        }
+        bank = temp;
+
     }
 
 
-    public Card buyCard(Card card){
+    public void buyCard(Card card) {
 
-        if (canBuyWhite(card) && canBuyBlue(card) && canBuyGreen(card) && canBuyRed(card) && canBuyBlack(card)){
+        if (canBuyWhite(card) && canBuyBlue(card) && canBuyGreen(card) && canBuyRed(card) && canBuyBlack(card)) {
             playerCanBuy = true;
         }
 
         if (playerCanBuy) {
             this.score += card.getPoints();  //score increased directly on buy
         }
-        return card;
+        System.out.println(Messages.CANT_BUY);
     }
 
 
-    private boolean canBuyWhite(Card card){
+    private boolean canBuyWhite(Card card) {
         if (card.getCostWhite() <= ownedMines[0]
                 || card.getCostWhite() <= ownedMines[0] + bank[0]
-                || card.getCostWhite() <= ownedMines[0] + bank[0] + bank[5]){
-            return  true;
+                || card.getCostWhite() <= ownedMines[0] + bank[0] + bank[5]) {
+            return true;
         }
         return false;
     }
 
-    private boolean canBuyBlue (Card card){
+    private boolean canBuyBlue(Card card) {
         if (card.getCostBlue() <= ownedMines[1]
                 || card.getCostBlue() <= ownedMines[1] + bank[1]
-                || card.getCostBlue() <= ownedMines[1] + bank[1] + bank[5]){
-            return  true;
+                || card.getCostBlue() <= ownedMines[1] + bank[1] + bank[5]) {
+            return true;
         }
         return false;
     }
 
-    private boolean canBuyGreen (Card card){
+    private boolean canBuyGreen(Card card) {
         if (card.getCostGreen() <= ownedMines[2]
                 || card.getCostGreen() <= ownedMines[2] + bank[2]
-                || card.getCostGreen() <= ownedMines[2] + bank[2] + bank[5]){
-            return  true;
+                || card.getCostGreen() <= ownedMines[2] + bank[2] + bank[5]) {
+            return true;
         }
         return false;
     }
 
-    private boolean canBuyRed (Card card){
+    private boolean canBuyRed(Card card) {
         if (card.getCostRed() <= ownedMines[3]
                 || card.getCostRed() <= ownedMines[3] + bank[3]
-                || card.getCostRed() <= ownedMines[3] + bank[3] + bank[5]){
-            return  true;
+                || card.getCostRed() <= ownedMines[3] + bank[3] + bank[5]) {
+            return true;
         }
         return false;
     }
 
-    private boolean canBuyBlack (Card card){
+    private boolean canBuyBlack(Card card) {
         if (card.getCostBlack() <= ownedMines[4]
                 || card.getCostBlack() <= ownedMines[4] + bank[4]
-                || card.getCostBlack() <= ownedMines[4] + bank[4] + bank[5]){
-            return  true;
+                || card.getCostBlack() <= ownedMines[4] + bank[4] + bank[5]) {
+            return true;
         }
         return false;
     }
--- src/academy/mindswap/utils/Messages.java
+++ src/academy/mindswap/utils/Messages.java
@@ -2,6 +2,9 @@ package academy.mindswap.utils;
 
 public class Messages {
 
+
+
+
     //Colors utilized in our game
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_BLUE = "\u001B[34m";
@@ -15,6 +18,19 @@ public class Messages {
     public static final String ANSI_CYAN = "\u001B[36m";
 
     public static final String ANSI_RESET = "\u001B[0m";
+
+    //PLAYER MESSAGES
+    public static final String IMPOSSIBLE_MOVE = ANSI_RED + "Not possible babe... " + ANSI_RESET;
+    public static final String CANT_RESERVE = ANSI_RED + "You can't reserve more than three cards " + ANSI_RESET;
+    public static final String CANT_BUY = ANSI_RED + "Not enough credits " + ANSI_RESET;
+    public static final String MORE_THAN_10 = ANSI_RED + "You can only have 10 Tokens  " + ANSI_RESET;
+    public static final String TOKEN_STACK_LOW = ANSI_RED + "Token stack is low. You can only pick one token of this kind " + ANSI_RESET;
+    public static final String GOLD_TOKEN_AWARDED = ANSI_YELLOW + "You just received a GOLDEN TOKEN " + ANSI_RESET;
+
+
+
+
+
 
     //psvm to test if colors are working, delete after
     public static void main(String[] args) {
