20efd9ee3b645be60a372f9b8a7093e6c760d1f8
Ricardo Paiva <ricardowrc@gmail.com>
2022-02-24 17:29:12 +0000

    PrintBoard finished v.0.1

--- src/academy/mindswap/cards/Mine.java
+++ src/academy/mindswap/cards/Mine.java
@@ -5,9 +5,6 @@ public class Mine extends Card{
     protected String color;
 
     public Mine(int costWhite, int costBlue, int costGreen, int costRed, int costBlack, int points, int tier, String color){
-        super(costWhite, costBlue, costGreen, costRed, costBlack, points, tier);
-        this.color = color;
+        super(costWhite, costBlue, costGreen, costRed, costBlack, points, tier, color);
     }
-
-
 }
